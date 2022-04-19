460871f186316f02105e0fd00a050f73212f4910
Fangyi Zhou <me@fangyi.io>
2022-04-19 23:47:31 +0100

    Implement layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Grid } from 'fomantic-ui-react';
+import { Container, Grid } from 'fomantic-ui-react';
 import Layout from './Layout';
 
 function App() {
   const layout = <Layout></Layout>;
   return (
-    <Grid>
-      <Grid.Row centered>
-        <h1>Factorio Nuclear Power Plant Calculator</h1>
-      </Grid.Row>
-      <Grid.Row centered>
-        <Grid.Column>{layout}</Grid.Column>
-        <Grid.Column>Analysis</Grid.Column>
-      </Grid.Row>
-    </Grid>
+    <Container>
+      <Grid columns={2} divided>
+        <Grid.Row centered>
+          <h1>Factorio Nuclear Power Plant Calculator</h1>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column>{layout}</Grid.Column>
+          <Grid.Column>Analysis</Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
   );
 }
 
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,15 +1,17 @@
-import { Grid } from 'fomantic-ui-react';
+import { Grid, Segment } from 'fomantic-ui-react';
 import React from 'react';
+import update from 'immutability-helper';
 
 type S = {
   layout: Array<Array<boolean>>;
 };
 
+const nuclearEmoji = '☢️';
 const maxCol = 3;
 const maxRow = 3;
-const emptyLayout = [
+const defaultLayout = [
   [false, false, false],
-  [false, false, false],
+  [false, true, false],
   [false, false, false],
 ];
 
@@ -17,32 +19,83 @@ class Layout extends React.Component<{}, S> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      layout: emptyLayout,
+      layout: defaultLayout,
     };
   }
 
   countNeighbour(rowIdx: number, cellIdx: number): number {
-    let count = 1;
     const layout = this.state.layout;
-    if (rowIdx >= 0 && layout[rowIdx - 1][cellIdx]) count++;
-    if (rowIdx < maxRow && layout[rowIdx + 1][cellIdx]) count++;
-    if (cellIdx >= 0 && layout[rowIdx][cellIdx - 1]) count++;
-    if (cellIdx < maxCol && layout[rowIdx][cellIdx + 1]) count++;
+    if (!layout[rowIdx][cellIdx]) {
+      return 0;
+    }
+    let count = 1;
+    console.log(rowIdx, cellIdx);
+    if (rowIdx > 0 && layout[rowIdx - 1][cellIdx]) count++;
+    if (rowIdx < maxRow - 1 && layout[rowIdx + 1][cellIdx]) count++;
+    if (cellIdx > 0 && layout[rowIdx][cellIdx - 1]) count++;
+    if (cellIdx < maxCol - 1 && layout[rowIdx][cellIdx + 1]) count++;
     return count;
+  }
+
+  handleClick(rowIdx: number, cellIdx: number) {
+    console.log(rowIdx, cellIdx);
+    if (rowIdx < 0 || rowIdx >= maxRow) return;
+    if (cellIdx < 0 || cellIdx >= maxCol) return;
+    this.setState(
+      update(this.state, {
+        layout: {
+          [rowIdx]: {
+            [cellIdx]: {
+              $set: !this.state.layout[rowIdx][cellIdx],
+            },
+          },
+        },
+      })
+    );
+    this.state.layout[rowIdx][cellIdx] = !this.state.layout[rowIdx][cellIdx];
+  }
+
+  renderCell(rowIdx: number, cellIdx: number) {
+    const count = this.countNeighbour(rowIdx, cellIdx);
+    const inner =
+      count === 0 ? (
+        <div></div>
+      ) : (
+        <div>
+          {nuclearEmoji}
+          <br />
+          {count + 'x'}
+        </div>
+      );
+    return (
+      <Segment style={{ minHeight: '70px' }}>
+        <div
+          onClick={() => {
+            this.handleClick(rowIdx, cellIdx);
+          }}
+          style={{ textAlign: 'center', height: '100%' }}
+        >
+          {inner}
+        </div>
+      </Segment>
+    );
   }
 
   render() {
     return (
-      <Grid celled columns={3}>
+      <Grid celled columns={maxCol}>
         {this.state.layout.map((row, rowIdx) => (
-          <Grid.Row centered key={`Row-${rowIdx}`}>
+          <Grid.Row stretched key={`Row-${rowIdx}`}>
             {row.map((cell, cellIdx) => (
               <Grid.Column key={`Cell-${rowIdx}-${cellIdx}`}>
-                {cell ? 1 + this.countNeighbour(rowIdx, cellIdx) : 0}
+                {this.renderCell(rowIdx, cellIdx)}
               </Grid.Column>
             ))}
           </Grid.Row>
         ))}
+        <Grid.Row centered>
+          Click on an empty cell to place a nuclear power plant.
+        </Grid.Row>
       </Grid>
     );
   }
